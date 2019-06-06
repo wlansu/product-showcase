20cca2fe7bfad936b7fd40f1341e013290a82bde
wouterlansu <wfrlansu@gmail.com>
2019-06-06 11:30:07 +0200

    added s3direct to urls

--- klanad_website/urls.py
+++ klanad_website/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from klanad import views
 
@@ -25,5 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.products, name="products"),
     path("product/<uuid:product_id>/", views.product, name="product"),
+    path('s3direct/', include('s3direct.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
