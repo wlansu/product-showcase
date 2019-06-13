1ea212e6b4563d5b545dc27e50524db3bd3c95f3
wouterlansu <wfrlansu@gmail.com>
2019-06-13 17:39:22 +0200

    Try using without the cache.

--- klanad_website/settings.py
+++ klanad_website/settings.py
@@ -129,11 +129,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
